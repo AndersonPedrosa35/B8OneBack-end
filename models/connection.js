12972b879a831a5d3366495f2d22493ceeacccca
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-25 20:04:49 -0300

    Inserir - Connection no Model - Criação da conecção do mongodb

---
+++ models/connection.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+let schema = null;
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+const { DB_NAME, DB_URL } = process.env
+
+function connection() {
+  return schema ? Promise.resolve(schema) :
+    MongoClient.connect(DB_URL, OPTIONS)
+    .then((conn) => conn.db(DB_NAME))
+    .then((db) => {
+      schema = db;
+      return schema;
+  })
+}
+
+module.exports = connection;
