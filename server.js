11983f2bb9af105df809b83eca937cef8758adac
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-26 19:11:05 -0300

    Inserir - server.js & package.json - Criação de um script para desenvolvedor iniciando com o nodemon, utilização do cors no servidor

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const productController = require('./controllers/productController');
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 const { PORT } = process.env;
 
