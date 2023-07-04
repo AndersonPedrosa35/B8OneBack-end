adf865030e115d00518683ad0f817faa35e88552
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2023-07-04 15:36:22 -0300

    REFACTOR

--- models/productModel.js
+++ models/productModel.js
@@ -1,23 +1,23 @@
-const connection = require('./connection');
-const { ObjectId } = require('mongodb')
+// const connection = require('./connection');
+// const { ObjectId } = require('mongodb')
 
-const getAll = async () => {
-  const data = await connection().then((db) => db.collection('products'));
-  return data.find().toArray();
-}
+// const getAll = async () => {
+//   const data = await connection().then((db) => db.collection('products'));
+//   return data.find().toArray();
+// }
 
-const createProduct = async (body) => {
-  const data = await connection().then((db) => db.collection('products'));
-  return data.insertOne(body);
-}
+// const createProduct = async (body) => {
+//   const data = await connection().then((db) => db.collection('products'));
+//   return data.insertOne(body);
+// }
 
-const deleteProductById = async (id) => {
-  const data = await connection().then((db) => db.collection('products'));
-  return data.deleteOne({_id: ObjectId(id)});
-}
+// const deleteProductById = async (id) => {
+//   const data = await connection().then((db) => db.collection('products'));
+//   return data.deleteOne({_id: ObjectId(id)});
+// }
 
-module.exports = {
-  getAll,
-  createProduct,
-  deleteProductById,
-}
+// module.exports = {
+//   getAll,
+//   createProduct,
+//   deleteProductById,
+// }
