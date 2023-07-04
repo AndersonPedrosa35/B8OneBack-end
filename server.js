39e3e419314ef211a24f8b89f7693bb7f46a2385
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2023-07-04 15:30:00 -0300

    fix: Atualiza a PORTA

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const { PORT } = process.env;
+const PORT = process.env?.PORT;
 
 app.get('/', (req, res) => {
   return res.status(200).send('Estamos conectados');
@@ -22,7 +22,7 @@ app.get('/products', productController.getAll);
 
 app.post('/products', productController.createProduct);
 
-app.listen(PORT, () => {
-  console.log(`Online na porta ${PORT}`);
+app.listen(PORT ? PORT : 3001, () => {
+  console.log(`Online na porta ${PORT ? PORT : 3001}`);
 })
 
