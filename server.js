079164517a402e41572cefdcd2c9899a7e533535
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2023-07-04 16:04:37 -0300

    REFACTOR

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const path = require('path');
 const productController = require('./controllers/productController');
+const app = express();
 
-const PORT = process.env.PORT || 5001;
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env?.PORT || 3000;
 
 app.get('/', (req, res) => {
-  return res.status(200).json({message: 'Estamos conectados'});
+  return res.status(200).send('Estamos conectados');
 });
+
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
 
 app.delete('/products', productController.deleteProductById)
 
